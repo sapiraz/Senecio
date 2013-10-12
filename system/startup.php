34fc10b7e5d029b0b32f11d8aec0870e077647b2
Raz Sapir <>
2013-10-12 14:49:22 +0200

    Include the navigator

--- system/startup.php
+++ system/startup.php
@@ -6,6 +6,7 @@ require_once(SYS_FOLDER . "includes/Template.php");
 require_once(SYS_FOLDER . "includes/Controller.php");
 require_once(SYS_FOLDER . "includes/Database.php");
 require_once(SYS_FOLDER . "includes/Language.php");
+require_once(SYS_FOLDER . "includes/Navigator.php");
 require_once(SYS_FOLDER . "includes/public_functions.php");
 
 
