769c3cb791acf00c55a5fa77512a628785a6b46b
Raz Sapir <>
2013-10-12 14:48:20 +0200

    Added navigator to control the site on its own.

--- system/includes/Navigator.php
+++ system/includes/Navigator.php
@@ -6,7 +6,23 @@
  */
 
 class Navigator {
-    //put your code here
+    //Lets take care of the site navigation and all that.
+    private $act;
+    public function __construct() {
+        $act = isset($_GET['act']) ? $_GET['act'] : "";
+        $act = preg_match_all("/(\w+\/\w+)/", $act) ? $act : DEFAULT_CONTROLLER;
+        $this->setAct($act);
+        
+    }
+    public function setAct($act){
+        $this->act = $act;
+    }
+    public function getAct(){
+        return $this->act;
+    }
+    public function __destruct(){
+        
+    }
 }
 
 ?>
