64b58ef805c1f542813fa67e8036b7c1f144ab1b
Raz Sapir <>
2013-10-12 14:49:08 +0200

    Added default controller setting

--- Config.php
+++ Config.php
@@ -9,6 +9,7 @@ define("SYSTEM_VERSION","0.41");
 define("SYSTEM_NAME","Senecio");
 define("DEFAULT_SITE_NAME","Senecio");
 define("SITE_HOME","/senecio/");
+define("DEFAULT_CONTROLLER","general/home");
 define("SYS_FOLDER",__DIR__ . "/system/");
 define("PUBLIC_LANGUAGES_FOLDER","display/languages/");
 define("PUBLIC_TEMPLATES_FOLDER","display/templates/");
